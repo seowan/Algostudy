e05982c8be0275616cb093c29171b010c1b368a3
seowan <seowan@LAPTOP-MTH1GR8K>
2020-09-16 16:35:55 +0900

    bj_10845_큐

--- 200805스터디문제/src/bj_10845_큐.java
+++ 200805스터디문제/src/bj_10845_큐.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class bj_10845 {
+public class bj_10845_ť {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
