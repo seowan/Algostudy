7a543e8840ec7bf74b8bb3d707f16aa68ff22d85
seowan <seowan@LAPTOP-MTH1GR8K>
2020-09-16 22:00:14 +0900

    bj_2467_용액

---
+++ 200916_스터디문제/src/bj_2467_용액.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class bj_2467_ฟ๋พื {
+	static int answer[] = new int[2];
+	static int list[], N, min = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		list = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			list[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(list);
+
+		int start = 0;
+		int end = N - 1;
+
+		while (start < end) {
+			if (Math.abs(list[start] + list[end]) < min) {
+				min = Math.abs(list[start] + list[end]);
+				answer[0] = list[start];
+				answer[1] = list[end];
+			}
+			if (list[start] + list[end] < 0)
+				start++;
+			else
+				end--;
+		}
+		System.out.println(answer[0] + " " + answer[1]);
+	}
+
+}
