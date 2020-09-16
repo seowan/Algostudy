043af9c22c92eb1bc03b2856c7c2766be1af1c52
seowan <seowan@LAPTOP-MTH1GR8K>
2020-09-16 16:44:06 +0900

    bj_10814_나이순정렬

---
+++ 200805스터디문제/src/bj_10814_나이순정렬.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class bj_10814_나이순정렬 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		String[][] person = new String[num][2];
+		for (int i = 0; i < num; i++) {
+			person[i][0] = sc.next();
+			person[i][1] = sc.next();
+		}
+
+		Arrays.sort(person, new Comparator<String[]>() {
+			public int compare(String[] p1, String[] p2) {
+				return Integer.compare(Integer.parseInt(p1[0]), Integer.parseInt(p2[0]));
+			}
+		});
+
+		for (int i = 0; i < person.length; i++) {
+			System.out.println(person[i][0] + " " + person[i][1]);
+		}
+
+	}
+
+}
