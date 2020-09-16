3635176d7fbe9eef709209354527b8f345cf4b85
seowan <seowan@LAPTOP-MTH1GR8K>
2020-09-16 16:32:36 +0900

    bj_2609

--- 200805스터디문제/src/bj_2609.java
+++ 200805스터디문제/src/bj_2609.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class bj_2609 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+
+		System.out.println(gcd(num1, num2));
+		System.out.println(lcm(num1, num2));
 	}
 
+	public static int gcd(int a, int b) {
+		while (b > 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+	}
+
+	public static int lcm(int a, int b) {
+		return (a * b) / gcd(a, b);
+	}
 }
