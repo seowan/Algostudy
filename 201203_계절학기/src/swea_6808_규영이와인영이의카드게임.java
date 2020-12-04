8504f04134b055f53127e6080be720f7f65dc1e5
seowan <seowan@LAPTOP-MTH1GR8K>
2020-12-04 14:12:16 +0900

    swea_6808_규영이와인영이의카드게임

--- 201203_계절학기/src/swea_6808_규영이와인영이의카드게임.java
+++ 201203_계절학기/src/swea_6808_규영이와인영이의카드게임.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class swea_3349_최솟값으로이동하기 {
+public class swea_6808_규영이와인영이의카드게임 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
