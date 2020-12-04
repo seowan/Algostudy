4652a4c6ceda862638fa506b26bf86859fd97c7a
seowan <seowan@LAPTOP-MTH1GR8K>
2020-12-04 14:09:35 +0900

    swea_3349_최솟값으로이동하기

---
+++ 201203_계절학기/src/swea_3349_최솟값으로이동하기.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class swea_3349_최솟값으로이동하기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
