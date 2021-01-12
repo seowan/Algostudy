879a8c8ebfa66711a8ba835696882cd3b3758026
seowan <seowan@LAPTOP-MTH1GR8K>
2021-01-12 22:33:19 +0900

    bj_2473_세용액

--- 210114_스터디문제/src/bj_2473_세용액.java
+++ 210114_스터디문제/src/bj_2473_세용액.java
@@ -21,19 +21,19 @@ public class bj_2473_세용액 {
 		Arrays.sort(water);//정렬
 		
 		for (int i = 0; i < N; i++) { //이게 용액 기준점으로 연산
-			int start = i+1;
-			int end = N-1;
-			while (start<end) {
-				if (Math.abs(water[i]+water[start]+water[end])<min) {
-					min = Math.abs(water[i]+water[start]+water[end]);
+			int start = i+1;//시작점
+			int end = N-1;//끝점
+			while (start<end) {//시작점이 끝점을 지나지않을때만 수행
+				if (Math.abs(water[i]+water[start]+water[end])<min) {//세용액합이 min보다 작을경우
+					min = Math.abs(water[i]+water[start]+water[end]);//min값 갱신
 					answer[0] = water[i];
 					answer[1] = water[start];
 					answer[2] = water[end];
 				}
 				
-				if (water[i]+water[start]+water[end]<0) {
-					start++;
-				}else end--;
+				if (water[i]+water[start]+water[end]<0) {//세용액합이 0보다 작으면
+					start++;//시작점을 다음으로 옮긴다
+				}else end--;//그렇지 않으면 끝점을 하나당긴다
 			}
 		}
 		
